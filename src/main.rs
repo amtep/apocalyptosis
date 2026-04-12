1f09903772b8ef79ee3570bf92288dd4ab25ea78
Richard Braakman <shuttlegit@gmail.com>
2026-04-12 12:45:57 +0300

    status bar and map

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,44 @@
-use bevy::DefaultPlugins;
+use bevy::color::palettes::css::{BLACK, WHITE};
+use bevy::prelude::*;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
-    commands.spawn(Sprite::from_image(
-        asset_server.load("textures/earth_night.jpg"),
+    commands.spawn((
+        Node {
+            flex_direction: FlexDirection::Column,
+            width: vw(100.0),
+            height: vh(100.0),
+            ..Default::default()
+        },
+        children![
+            (
+                Node {
+                    width: vw(100.0),
+                    height: vh(5.0),
+                    border: UiRect {
+                        left: vw(0.0),
+                        right: vw(0.0),
+                        top: vh(0.5),
+                        bottom: vh(0.5),
+                    },
+                    ..Default::default()
+                },
+                BorderColor::all(WHITE),
+                BackgroundColor(BLACK.into()),
+            ),
+            (
+                ImageNode {
+                    image: asset_server.load("textures/earth_night.jpg"),
+                    image_mode: NodeImageMode::Stretch,
+                    ..Default::default()
+                },
+                Node {
+                    width: vw(100.0),
+                    height: vh(95.0),
+                    ..Default::default()
+                },
+            )
+        ],
     ));
 }
 
