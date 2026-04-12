fa00d5b907642b27ad83cd827caa3090ec9fc16b
Richard Braakman <shuttlegit@gmail.com>
2026-04-12 11:34:42 +0300

    show background image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::DefaultPlugins;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+    commands.spawn(Sprite::from_image(
+        asset_server.load("textures/earth_night.jpg"),
+    ));
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
 }
