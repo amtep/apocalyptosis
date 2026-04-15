fbfab1e3c0fccdeecd50e24ef7cf683d12f34fb5
Richard Braakman <shuttlegit@gmail.com>
2026-04-15 21:14:39 +0300

    ui: prettier region labels

--- assets/text/en-US/regions.ftl
+++ assets/text/en-US/regions.ftl
@@ -1,2 +1,7 @@
-north_america = North America
-south_america = South America
+region_north_america = North America
+region_south_america = South America
+region_europe = Europe
+region_africa = Africa
+region_near_east = Near East
+region_far_east = Far East
+region_australia = Australia
--- src/regions.rs
+++ src/regions.rs
@@ -1,6 +1,10 @@
 use std::collections::{HashMap, HashSet};
 
-use bevy::{asset::AsAssetId, prelude::*};
+use bevy::{
+    asset::AsAssetId,
+    color::palettes::css::{BLACK, WHITE},
+    prelude::*,
+};
 use bevy_common_assets::toml::TomlAssetPlugin;
 use serde::Deserialize;
 
@@ -48,36 +52,44 @@ fn watch_regions(
     mut commands: Commands,
     assets: Res<Assets<RegionsAsset>>,
     regions: Single<&Regions, AssetChanged<Regions>>,
-    mut q_ui: Query<(Entity, &mut Text, &mut Node, &RegionUi)>,
+    mut q_ui_node: Query<(Entity, &mut Node, &RegionUi)>,
     mapui: Single<Entity, With<MapUi>>,
 ) {
-    info!("regions changed");
+    info!("Regions changed");
     if let Some(settings) = assets.get(&regions.0) {
         let mut seen: HashSet<&str> = HashSet::default();
-        for (region_e, mut text, mut node, region) in &mut q_ui {
+        for (region_e, mut node, region) in &mut q_ui_node {
             if let Some(region_settings) = settings.0.get(&region.0) {
                 seen.insert(&region.0);
                 node.left = percent(region_settings.x);
                 node.top = percent(region_settings.y);
-                text.0 = region.0.clone();
             } else {
                 commands.entity(region_e).despawn();
             }
         }
         for (key, region_settings) in &settings.0 {
             if !seen.contains(&key.as_ref()) {
-                commands.spawn((
-                    Text::new("".to_string()),
-                    Node {
-                        position_type: PositionType::Absolute,
-                        left: percent(region_settings.x),
-                        top: percent(region_settings.y),
-                        ..default()
-                    },
-                    TextKey(key.clone()),
-                    RegionUi(key.clone()),
-                    ChildOf(*mapui),
-                ));
+                let text_key = format!("region_{key}");
+                commands
+                    .spawn((
+                        Node {
+                            position_type: PositionType::Absolute,
+                            left: percent(region_settings.x),
+                            top: percent(region_settings.y),
+                            border: UiRect::all(px(2)),
+                            border_radius: BorderRadius::all(px(10)),
+                            padding: UiRect::all(px(10)),
+                            align_self: AlignSelf::Center,
+                            ..default()
+                        },
+                        BorderColor::all(WHITE),
+                        BackgroundColor(BLACK.into()),
+                        RegionUi(key.clone()),
+                        ChildOf(*mapui),
+                    ))
+                    .with_children(|parent| {
+                        parent.spawn((Text::new("".to_string()), TextKey(text_key)));
+                    });
             }
         }
     }
