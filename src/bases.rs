0ba747bd5196ef00224a96a4a0a5481c063e3cfa
Richard Braakman <shuttlegit@gmail.com>
2026-04-20 09:26:00 +0300

    refactor: straighten out bases vs basetypes naming

--- src/bases.rs
+++ src/bases.rs
@@ -14,35 +14,35 @@ use crate::{
     text::TextKey,
 };
 
-const BASES_ASSET_PATH: &str = "data/bases.toml";
+const BASETYPES_ASSET_PATH: &str = "data/basetypes.toml";
 
 pub struct BasesPlugin;
 
 impl Plugin for BasesPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(TomlAssetPlugin::<BasesAsset>::new(&["toml"]))
-            .add_systems(Startup, setup_basetypes);
+        app.add_plugins(TomlAssetPlugin::<BasetypesAsset>::new(&["toml"]))
+            .add_systems(Startup, startup);
     }
 }
 
 #[derive(Deserialize, Asset, TypePath)]
-pub struct BasesAsset(HashMap<String, BaseTypeSettings>);
+struct BasetypesAsset(HashMap<String, BasetypeSettings>);
 
 #[derive(Deserialize)]
 #[allow(dead_code)] // TODO
 #[serde(rename_all = "kebab-case")]
-struct BaseTypeSettings {
+struct BasetypeSettings {
     people: isize,
     cost_per_day: FundsAmount,
     initial_cost: FundsAmount,
 }
 
 #[derive(Resource)]
-pub struct BaseTypesResource(Handle<BasesAsset>);
+pub struct BasetypesHandle(Handle<BasetypesAsset>);
 
-fn setup_basetypes(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.insert_resource(BaseTypesResource(
-        asset_server.load::<BasesAsset>(BASES_ASSET_PATH),
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(BasetypesHandle(
+        asset_server.load::<BasetypesAsset>(BASETYPES_ASSET_PATH),
     ));
     commands.add_observer(spawn_base);
     commands.add_observer(new_game_bases);
@@ -53,10 +53,10 @@ fn setup_basetypes(mut commands: Commands, asset_server: Res<AssetServer>) {
 #[derive(Component)]
 pub struct Base;
 
-/// The `String` is the key for the base type in the BaseTypes asset.
+/// The `String` is the key for the base type in the `Basetypes` asset.
 #[derive(Component)]
 #[allow(dead_code)] // TODO
-pub struct BaseType(pub String);
+pub struct Basetype(pub String);
 
 #[derive(Event)]
 pub struct SpawnBaseDirective {
@@ -67,8 +67,8 @@ pub struct SpawnBaseDirective {
 fn spawn_base(
     event: On<SpawnBaseDirective>,
     mut commands: Commands,
-    assets: Res<Assets<BasesAsset>>,
-    base_types: Res<BaseTypesResource>,
+    assets: Res<Assets<BasetypesAsset>>,
+    base_types: Res<BasetypesHandle>,
     asset_server: Res<AssetServer>,
 ) {
     let Some(base_types) = assets.get(&base_types.0) else {
@@ -87,7 +87,7 @@ fn spawn_base(
     commands
         .spawn((
             Base,
-            BaseType(event.base_type.clone()),
+            Basetype(event.base_type.clone()),
             Expense(settings.cost_per_day, ExpenseCategory::Bases),
             ChildOf(event.region),
             Node {
