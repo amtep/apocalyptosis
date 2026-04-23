8146786997e7e51ee8de6605fab0ddde26be6042
Richard Braakman <shuttlegit@gmail.com>
2026-04-23 11:43:33 +0300

    fix: add missing file to previous commit

---
+++ src/ui/buttons.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+
+use crate::constants::ui::{MENU_BACKGROUND, MENU_HOVER_BACKGROUND, MENU_PRESSED_BACKGROUND};
+
+pub fn update_button_backgrounds(
+    mut q: Query<(&Interaction, &mut BackgroundColor), Changed<Interaction>>,
+) {
+    for (interaction, mut background_color) in &mut q {
+        match *interaction {
+            Interaction::Pressed => {
+                *background_color = MENU_PRESSED_BACKGROUND.into();
+            }
+            Interaction::Hovered => {
+                *background_color = MENU_HOVER_BACKGROUND.into();
+            }
+            Interaction::None => {
+                *background_color = MENU_BACKGROUND.into();
+            }
+        }
+    }
+}
