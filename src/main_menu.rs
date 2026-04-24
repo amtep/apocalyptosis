60e745f81c49708e265c3e7ea943f2e5f7498de5
Michael Yang <michaely4ng@proton.me>
2026-04-24 17:37:31 +1000

    feat: rework main menu button click

--- src/main_menu.rs
+++ src/main_menu.rs
@@ -7,21 +7,11 @@ use crate::{
 };
 
 pub fn plugin(app: &mut App) {
-    app.add_systems(OnEnter(GameState::MainMenu), setup)
-        .add_systems(
-            Update,
-            listen_button_press.run_if(in_state(GameState::MainMenu)),
-        );
-}
-
-#[derive(Component)]
-enum MenuAction {
-    NewGame,
-    Quit,
+    app.add_systems(OnEnter(GameState::MainMenu), setup);
 }
 
 fn setup(mut commands: Commands, bundle: Res<FluentBundleWrapper>, asset_server: Res<AssetServer>) {
-    let button = |key, action| {
+    let button = |key| {
         (
             Button,
             Node {
@@ -37,7 +27,6 @@ fn setup(mut commands: Commands, bundle: Res<FluentBundleWrapper>, asset_server:
             },
             BorderColor::all(Color::WHITE),
             BackgroundColor(MENU_BACKGROUND.into()),
-            action,
             children![(
                 TextFont {
                     font: asset_server.load(FONT_PATH),
@@ -48,24 +37,26 @@ fn setup(mut commands: Commands, bundle: Res<FluentBundleWrapper>, asset_server:
             )],
         )
     };
-    commands.spawn((
-        DespawnOnExit(GameState::MainMenu),
-        Node {
-            width: percent(100),
-            height: percent(100),
-            align_items: AlignItems::Start,
-            justify_content: JustifyContent::Start,
-            flex_direction: FlexDirection::Column,
-            row_gap: px(40),
-            ..default()
-        },
-        ImageNode {
-            image: asset_server.load(TEXTURE_EARTH_BACKGROUND),
-            image_mode: NodeImageMode::Stretch,
-            ..default()
-        },
-        children![
-            (
+    commands
+        .spawn((
+            DespawnOnExit(GameState::MainMenu),
+            Node {
+                width: percent(100),
+                height: percent(100),
+                align_items: AlignItems::Start,
+                justify_content: JustifyContent::Start,
+                flex_direction: FlexDirection::Column,
+                row_gap: px(40),
+                ..default()
+            },
+            ImageNode {
+                image: asset_server.load(TEXTURE_EARTH_BACKGROUND),
+                image_mode: NodeImageMode::Stretch,
+                ..default()
+            },
+        ))
+        .with_children(|parent| {
+            parent.spawn((
                 Node {
                     width: percent(100),
                     height: px(200),
@@ -83,38 +74,25 @@ fn setup(mut commands: Commands, bundle: Res<FluentBundleWrapper>, asset_server:
                     },
                     TextShadow::default(),
                 )],
-            ),
-            (
-                Node {
+            ));
+            parent
+                .spawn(Node {
                     flex_direction: FlexDirection::Column,
                     align_self: AlignSelf::Center,
                     row_gap: px(40),
                     ..default()
-                },
-                children![
-                    button("menu-button-new-game", MenuAction::NewGame),
-                    button("menu-button-quit", MenuAction::Quit),
-                ]
-            ),
-        ],
-    ));
-}
-
-fn listen_button_press(
-    q: Query<(&Interaction, &MenuAction), (Changed<Interaction>, With<Button>)>,
-    mut game_state: ResMut<NextState<GameState>>,
-    mut exit: MessageWriter<AppExit>,
-) {
-    for (interaction, menu_action) in &q {
-        if *interaction == Interaction::Pressed {
-            match menu_action {
-                MenuAction::NewGame => {
-                    game_state.set(GameState::Main);
-                }
-                MenuAction::Quit => {
-                    exit.write(AppExit::Success);
-                }
-            }
-        }
-    }
+                })
+                .with_children(|parent| {
+                    parent.spawn(button("menu-button-new-game")).observe(
+                        |_: On<Pointer<Click>>, mut game_state: ResMut<NextState<GameState>>| {
+                            game_state.set(GameState::Main);
+                        },
+                    );
+                    parent.spawn(button("menu-button-quit")).observe(
+                        |_: On<Pointer<Click>>, mut exit: MessageWriter<AppExit>| {
+                            exit.write(AppExit::Success);
+                        },
+                    );
+                });
+        });
 }
